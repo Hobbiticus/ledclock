f07b895bbf57ec21f1614ec1b37c92e2295fabb0
Chris Weiland <yeahno@nope.edu>
2026-09-05 12:32:26 -0400

    networking stuff

--- src/ledclock.cpp
+++ src/ledclock.cpp
@@ -1,4 +1,11 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <ezTime.h>
+#include "passwd.h"
+
+#define DO_NETWORK
+
+Timezone myTZ;
 
 #define DATA_PIN 1
 #define CLOCK_PIN 2
@@ -32,10 +39,50 @@ void setup()
   pinMode(DATA_PIN, OUTPUT);
   pinMode(CLOCK_PIN, OUTPUT);
   digit = 0;
+
+#ifdef DO_NETWORK
+    //connect to WiFi
+  while (true)
+  {
+    Serial.println("Connecting to WiFi...");
+    WiFi.begin(MY_SSID, MY_WIFI_PASSWORD);
+    Serial.println("Waiting for wifi to connect...");
+    uint8_t result = WiFi.waitForConnectResult();
+    Serial.printf("result = %hhu\n", result);
+    if (result == WL_CONNECTED)
+    {
+      Serial.println("Connected to WiFi!");
+      break;
+    }
+    WiFi.disconnect(true);
+  }
+
+  Serial.println("Waiting for time sync...");
+  int startTime = millis();
+  waitForSync();
+  Serial.printf("Sync took %d ms\n", (int)(millis() - startTime));
+  myTZ.setLocation("America/New_York");
+#endif
 }
 
 void loop()
 {
+#ifdef DO_NETWORK
+  events(); //for ezTime
+
+  time_t now = myTZ.now();
+  uint8_t hour = myTZ.hour(now);
+  uint8_t minute = myTZ.minute(now);
+  uint8_t second = myTZ.second(now);
+
+  unsigned char dots = second % 2 == 0 ? DOT : 0;
+  shiftIn(DATA_PIN, CLOCK_PIN, NumberMap[second / 10] | dots);
+  shiftIn(DATA_PIN, CLOCK_PIN, NumberMap[second % 10] | dots);
+  delay(250);
+  return;
+
+  #else
+
   unsigned char dots = digit % 2 == 0 ? DOT : 0;
   shiftIn(DATA_PIN, CLOCK_PIN, NumberMap[digit] | dots);
   shiftIn(DATA_PIN, CLOCK_PIN, NumberMap[digit] | dots);
@@ -45,4 +92,5 @@ void loop()
     digit = 0;
 
     delay(1000);
+#endif
 }
